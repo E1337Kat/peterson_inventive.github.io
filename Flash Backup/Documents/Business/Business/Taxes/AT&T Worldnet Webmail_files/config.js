8b935c0dc4fecdef41516d452cff81334ba4d159
e1337kat <e1337kat@hotmail.com>
2019-09-16 13:02:13 -0400

    don't add unnecessary files.

--- Flash Backup/Documents/Business/Business/Taxes/AT&T Worldnet Webmail_files/config.js
+++
@@ -1,6 +0,0 @@
-///      Copyright (c) 2005 AT&T. All Rights Reserved
-///      THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF AT&T
-///      The copyright notice above does not evidence any
-///      actual or intended publication of such source code.
-var xl={sR:"http://www.att.net/",sS:"http://calendar.att.net/",sT:"https://memberservices.att.net/msws/EmailPwd",sU:"https://memberservices.att.net/msws/EmailParental",sV:"https://memberservices.att.net/msws/EmailFwd",sW:"https://memberservices.att.net/msws/",Km:"https://publish.att.net/",sX:"http://www.att.net/cgi-bin/goto?app=webnews&path=/webnews-cgi-bin/list_art.pl%3fnewsgroup=worldnet.help.service-issues.mail-server%26ntitle=Mail%2520Server%26ncat=Service",sY:"http://webmail.att.net/wmc/v/wm",s0:"https://webauth.att.net/auth/webmail/login?action=logout",KZ:"http://webmail.att.net/",w9:"https://webauth.att.net/auth/webmail/login",KP:"choose.html",sZ:"/wmc/n/wm/",KQ:'/cgi-bin/rss/putopml.cgi',s1:10,vL:1,vN:1174052587,wN:"worldnet",x2:"default",wW:120,wX:30,JM:".att.net",xm:-1,Kh:30,yT:'/cgi-bin/rss/getopml.cgi',DT:'/cgi-bin/rss/getfeed.cgi',KR:'/cgi-bin/rss/getchoice.cgi',KS:'/cgi-bin/rss/extractfeeds.cgi',KT:'/cgi-bin/rss/getkeywords.cgi',Hb:60,Ki:5,KU:"Previously Removed Feeds"}
-;
